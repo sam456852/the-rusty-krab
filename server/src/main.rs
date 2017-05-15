436f3cc438c7e631b219f7dda5d926d74e16bd1b
Daniel Letscher <dpletscher@outlook.com>
2017-05-14 22:34:44 -0500

    initial simple serverside handling and responding to requests

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,9 +2,35 @@ extern crate iron;
 
 use iron::prelude::*;
 use iron::status;
+use std::fs::OpenOptions;
+use std::io::{Write, Read, Seek, SeekFrom};
 
 fn main() {
-    Iron::new(|_: &mut Request| {
-        Ok(Response::with((status::Ok, "Hello world!")))
-    }).http("localhost:3000").unwrap();
+    println!("Welcome to Rust chat!");
+    Iron::new(parse_request).http("localhost:3000").unwrap();
+}
+
+fn parse_request(request: &mut Request) -> IronResult<Response> {
+    let mut body = String::new();
+    request
+        .body
+        .read_to_string(&mut body)
+        .map_err(|e| IronError::new(e, (status::InternalServerError, "Error reading request")))?;
+    let response_body = write_log(body);
+    Ok(Response::with((status::Ok, response_body)))
+}
+
+fn write_log(mut new_message: String) -> String {
+    let mut file = OpenOptions::new()
+                    .read(true)
+                    .create(true)
+                    .append(true)
+                    .open("messages.txt")
+                    .unwrap();
+    new_message.push_str("\n");
+    file.write_all(new_message.as_bytes()).unwrap();
+    file.seek(SeekFrom::Start(0)).unwrap();
+    let mut messages = String::new();
+    file.read_to_string(&mut messages).unwrap();
+    return messages;
 }
