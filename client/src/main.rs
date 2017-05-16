9c10e66e209d8d0f93694215736abb5afb193b2f
ChrisHartman <chris@familyisp.com>
2017-05-16 13:40:48 -0500

    Add rudimentary connection between client and server

--- client/src/main.rs
+++ client/src/main.rs
@@ -3,20 +3,10 @@
 //! A simple text file viewer
 
 extern crate gtk;
-extern crate iron;
-extern crate router;
-extern crate rustc_serialize;
+extern crate hyper;
 
-use std::fs::File;
-use std::fs::OpenOptions;
-use std::io::{Write, Read, Seek, SeekFrom, BufReader};
-
-use iron::prelude::*;
-use iron::status;
-
-use router::Router;
-use rustc_serialize::json;
-
+use std::io::Read;
+use hyper::Client;
 use gtk::prelude::*;
 use gtk::Builder;
 
@@ -25,8 +15,6 @@ pub fn main() {
         println!("Failed to initialize GTK.");
         return;
     }
-
-
 
 
     let glade_src = include_str!("rusty_chat.glade");
@@ -44,70 +32,37 @@ pub fn main() {
     let text_view: gtk::TextView = builder.get_object("text_view").unwrap();
 
 
-    let window1 = window.clone();
-    let chat_view1 = chat_view.clone();
-    let text_view1 = text_view.clone();
     log_in_button.connect_clicked(move |_| {
-
-        let file_chooser = gtk::FileChooserDialog::new(
-            Some("Open File"), Some(&window1), gtk::FileChooserAction::Open);
-
-        file_chooser.add_buttons(&[
-            ("Open", gtk::ResponseType::Ok.into()),
-            ("Cancel", gtk::ResponseType::Cancel.into()),
-        ]);
-
-        if file_chooser.run() == gtk::ResponseType::Ok.into() {
-            let filename = file_chooser.get_filename().unwrap();
-            let file = File::open(&filename).unwrap();
-
-            let mut reader = BufReader::new(file);
-            let mut contents = String::new();
-            let _ = reader.read_to_string(&mut contents);
-
-            chat_view1.get_buffer().unwrap().set_text(&contents);
-        }
-
-        file_chooser.destroy();
+        
+        // Not yet implemented
     });
 
-    let window2 = window.clone();
-    let chat_view2 = chat_view.clone();
-    let text_view2 = text_view.clone();
     new_chat_button.connect_clicked(move |_| {
 
         // Not yet implemented
     });
 
-    let window3 = window.clone();
-    let chat_view3 = chat_view.clone();
-    let text_view3 = text_view.clone();
     switch_chat_button.connect_clicked(move |_| {
 
         // Not yet implemented
     });
 
-    let window4 = window.clone();
-    let chat_view4 = chat_view.clone();
-    let text_view4 = text_view.clone();
+    let chat_window = chat_view.clone();
+    let sent_message_view = text_view.clone();
     send_button.connect_clicked(move |_| {
 
+        let current_message_buffer = sent_message_view.get_buffer().unwrap();
+        let start = current_message_buffer.get_start_iter();
+        let end = current_message_buffer.get_end_iter();
+        let current_text = current_message_buffer.get_text(&start, &end, true).unwrap();
 
-        let mut current_log_buffer = chat_view4.get_buffer().unwrap();
-        let mut start = current_log_buffer.get_start_iter();
-        let mut end = current_log_buffer.get_end_iter();
-        let mut currentLog = current_log_buffer.get_text(&start, &end, true).unwrap();
+        let client = Client::new();
+        let mut response = client.post("http://localhost:3000/").body(current_text.as_str()).send().unwrap();
+        let mut body = String::new();
+        response.read_to_string(&mut body).unwrap();
 
-        let mut current_message_buffer = text_view4.get_buffer().unwrap();
-        start = current_message_buffer.get_start_iter();
-        end = current_message_buffer.get_end_iter();
-        let mut currentText = current_message_buffer.get_text(&start, &end, true).unwrap();
-
-        let new_log = currentLog.as_str().to_owned() + currentText.as_str() + "\n";
-
-        chat_view4.get_buffer().unwrap().set_text(new_log.as_str());
-
-        text_view4.get_buffer().unwrap().set_text("");
+        chat_window.get_buffer().unwrap().set_text(body.as_str());
+        sent_message_view.get_buffer().unwrap().set_text("");
     });
 
     window.connect_delete_event(|_, _| {
